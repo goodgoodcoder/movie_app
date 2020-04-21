b55d4d5b6e0568d8b9c850ec26060696d8f1f84b
jongyunjeong <jongyunj@icloud.com>
2020-04-21 09:33:32 +0900

    Updates

---
+++ precache-manifest.9ddcbe6d91f7a831646a2d2d54a7ea29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c092bead5d17cfc72628a26237a5a886",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0a598906b673e339d85e",
+    "url": "/movie_app/static/css/main.0ac29744.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a598906b673e339d85e",
+    "url": "/movie_app/static/js/main.b94b2f99.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
